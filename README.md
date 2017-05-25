0b54fbb5f08b575a69421db676c6b50da7c29a32
Alessandro Angelino <alessandro.angelino@arm.com>
2017-05-25 17:53:53 +0100

    Add support for the DISCO_F429ZI target
    Fixes #18: "mbed-os-example-uvisor-number-store doesn't work on DISCO_F429ZI"

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ Supported devices:
 | Target            | Toolchain | Baud rate |
 |-------------------|-----------|-----------|
 | `K64F`            | `GCC_ARM` | 9600      |
+| `DISCO_F429ZI`    | `GCC_ARM` | 9600      |
 | `EFM32GG_STK3700` | `GCC_ARM` | 9600      |
 
 Latest release: [mbed-os-5.4.x](https://github.com/ARMmbed/mbed-os-example-uvisor/releases/tag/latest). Tested with [mbed-cli v1.0.0](https://github.com/ARMmbed/mbed-cli/releases/tag/1.0.0).
--- source/main-hw.h
+++ source/main-hw.h
@@ -73,6 +73,37 @@ extern Serial shared_pc;
         {(void *) 0x42000000, 0x2000000,       UVISOR_TACLDEF_PERIPH}, \
     }
 
+#elif defined(TARGET_DISCO_F429ZI)
+
+#define MAIN_LED           LED1
+#define SECURE_LED         LED2
+#define LED_ON             true
+#define LED_OFF            false
+#define SECURE_SWITCH      USER_BUTTON
+#define SECURE_SWITCH_PULL PullDown
+#define SHARED_SERIAL_BAUD 9600
+
+#define MAIN_ACL(acl_list_name) \
+    static const UvisorBoxAclItem acl_list_name[] = {     \
+        {GPIOA,               sizeof(*GPIOA),  UVISOR_TACLDEF_PERIPH}, \
+        {GPIOB,               sizeof(*GPIOB),  UVISOR_TACLDEF_PERIPH}, \
+        {GPIOC,               sizeof(*GPIOC),  UVISOR_TACLDEF_PERIPH}, \
+        {GPIOD,               sizeof(*GPIOD),  UVISOR_TACLDEF_PERIPH}, \
+        {GPIOE,               sizeof(*GPIOE),  UVISOR_TACLDEF_PERIPH}, \
+        {RTC,                 sizeof(*RTC),    UVISOR_TACLDEF_PERIPH}, \
+        {TIM5,                sizeof(*TIM5),   UVISOR_TACLDEF_PERIPH}, \
+        {USART1,              sizeof(*USART1), UVISOR_TACLDEF_PERIPH}, \
+        {I2C1,                sizeof(*I2C1),   UVISOR_TACLDEF_PERIPH}, \
+        {SPI1,                sizeof(*SPI1),   UVISOR_TACLDEF_PERIPH}, \
+        {RCC,                 sizeof(*RCC),    UVISOR_TACLDEF_PERIPH}, \
+        {FLASH,               sizeof(*FLASH),  UVISOR_TACLDEF_PERIPH}, \
+        {PWR,                 sizeof(*PWR),    UVISOR_TACLDEF_PERIPH}, \
+        {EXTI,                sizeof(*EXTI),   UVISOR_TACLDEF_PERIPH}, \
+        {GPIOG,               sizeof(*GPIOG),  UVISOR_TACLDEF_PERIPH}, \
+        {SYSCFG,              sizeof(*SYSCFG), UVISOR_TACLDEF_PERIPH}, \
+        {(void *) 0x42000000, 0x01000000,      UVISOR_TACLDEF_PERIPH}, \
+    }
+
 #else /* Target-specific settings */
 
 #error "Unsupported target. Checkout the README.md file for the list of supported targets for this app."
